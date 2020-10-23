5ce84d50cf5f5d16034308f1bedcc206638b7a11
Andrew Klychkov <aaklychkov@mail.ru>
2020-10-23 10:39:47 +0300

    Fix CI token

--- tests/utils/shippable/shippable.sh
+++ tests/utils/shippable/shippable.sh
@@ -168,7 +168,7 @@ function cleanup
                         -f "${file}" \
                         -F "${flags}" \
                         -n "${test}" \
-                        -t 20636cf5-4d6a-4b9a-8d2d-6f22ebbaa752 \
+                        -t 00c0f9fa-ac1b-43d3-addf-99de803232c1 \
                         -X coveragepy \
                         -X gcov \
                         -X fix \
