a259887629e5ea7ff1e41d99d176e35dd76051f5
Marcos Diez <marcos@unitron.com.br>
2021-12-14 22:44:54 -0300

    fix reconnect()

--- plugins/modules/postgresql_info.py
+++ plugins/modules/postgresql_info.py
@@ -545,13 +545,16 @@ class PgDbConn(object):
         self.db_conn = None
         self.cursor = None
 
-    def connect(self):
+    def connect(self, fail_on_conn=True):
         """Connect to a PostgreSQL database and return a cursor object.
 
         Note: connection parameters are passed by self.module object.
         """
         conn_params = get_conn_params(self.module, self.module.params, warn_db_default=False)
-        self.db_conn = connect_to_db(self.module, conn_params)
+        self.db_conn = connect_to_db(self.module, conn_params, fail_on_conn=fail_on_conn)
+        if self.db_conn is None:
+          # only happens if fail_on_conn is False and there actually was an issue connecting to the DB
+          return None
         return self.db_conn.cursor(cursor_factory=DictCursor)
 
     def reconnect(self, dbname):
@@ -560,10 +563,14 @@ class PgDbConn(object):
         Arguments:
             dbname (string): Database name to connect to.
         """
+        if self.db_conn is not None:
         self.db_conn.close()
 
+        # the lines below seem redudant but they are actually needed for connect to work as expected
+        self.module.params['db'] = dbname
         self.module.params['database'] = dbname
-        return self.connect()
+        self.module.params['login_db'] = dbname
+        return self.connect(fail_on_conn=False)
 
 
 class PgClusterInfo(object):
@@ -1012,6 +1019,13 @@ class PgClusterInfo(object):
 
         for datname in db_dict:
             self.cursor = self.db_obj.reconnect(datname)
+            if self.cursor is None:
+              # that means we don't have permission to access these database
+              db_dict[datname]['namespaces'] = {}
+              db_dict[datname]['extensions'] = {}
+              db_dict[datname]['languages'] = {}
+              db_dict[datname]['error'] = "could not connect to the database (probably permission issue)"
+              continue
             db_dict[datname]['namespaces'] = self.get_namespaces()
             db_dict[datname]['extensions'] = self.get_ext_info()
             db_dict[datname]['languages'] = self.get_lang_info()
