ad267786dea8a68f3290afc9713112a532a2df36
Andrew Klychkov <aklychko@redhat.com>
2021-12-20 09:02:13 +0300

    Update plugins/modules/postgresql_ping.py
    Co-authored-by: Douglas J Hunley <doug.hunley@gmail.com>

--- plugins/modules/postgresql_ping.py
+++ plugins/modules/postgresql_ping.py
@@ -68,7 +68,7 @@ EXAMPLES = r'''
     that:
     - result.is_available == yes
 
-# You can use the variable with another task
+# You can use the registered result with another task
 - name: This task should be executed only if the server is available
   ...
   when: result.is_available == yes
