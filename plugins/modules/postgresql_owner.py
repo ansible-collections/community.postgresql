d6df88d5083022b86ee2e9355358296139d013d8
RealGreenDragon <14246920+RealGreenDragon@users.noreply.github.com>
2023-08-09 20:58:36 +0200

    added operator class support

--- plugins/modules/postgresql_owner.py
+++ plugins/modules/postgresql_owner.py
@@ -34,7 +34,7 @@ options:
     - I(obj_type=procedure) and I(obj_type=routine) are available since PostgreSQL 11.
     type: str
     choices: [ database, function, matview, sequence, schema, table, tablespace, view, procedure,
-               type, aggregate, routine, language, domain, collation, conversion, operator ]
+               type, aggregate, routine, language, domain, collation, conversion, operator, operator_class ]
     aliases:
     - type
   reassign_owned_by:
@@ -323,6 +323,9 @@ class PgOwnership(object):
         elif obj_type == 'operator':
             self.__set_operator_owner()
 
+        elif obj_type == 'operator_class':
+            self.__set_operator_class_owner()
+
     def __is_owner(self):
         """Return True if self.role is the current object owner."""
         if self.obj_type == 'table':
@@ -404,6 +407,12 @@ class PgOwnership(object):
             query = ("SELECT 1 FROM pg_operator AS o "
                      "JOIN pg_roles AS r ON o.oprowner = r.oid "
                      "WHERE o.oprname = %(obj_name)s "
+                     "AND r.rolname = %(role)s")
+
+        elif self.obj_type == 'operator_class':
+            query = ("SELECT 1 FROM pg_opclass AS o "
+                     "JOIN pg_roles AS r ON o.opcowner = r.oid "
+                     "WHERE o.opcname = %(obj_name)s "
                      "AND r.rolname = %(role)s")
 
         if self.obj_type in ('function', 'aggregate', 'procedure', 'routine'):
@@ -518,6 +527,12 @@ class PgOwnership(object):
                                                      self.role)
         self.changed = exec_sql(self, query, return_bool=True)
 
+    def __set_operator_class_owner(self):
+        """Set the operator class owner."""
+        query = 'ALTER OPERATOR CLASS %s OWNER TO "%s"' % (pg_quote_identifier(self.obj_name, 'function'),
+                                                           self.role)
+        self.changed = exec_sql(self, query, return_bool=True)
+
     def __role_exists(self, role):
         """Return True if role exists, otherwise return False."""
         query_params = {'role': role}
@@ -531,7 +546,7 @@ class PgOwnership(object):
 
 VALID_OBJ_TYPES = ('database', 'function', 'matview', 'sequence', 'schema', 'table', 'tablespace', 'view',
                    'procedure', 'type', 'aggregate', 'routine', 'language', 'domain', 'collation', 'conversion',
-                   'operator',)
+                   'operator', 'operator_class',)
 
 
 def main():
