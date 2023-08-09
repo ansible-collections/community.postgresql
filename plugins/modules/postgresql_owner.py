ea0d8be90fd6e0d3768737c24b93c7de5c52c0f3
RealGreenDragon <14246920+RealGreenDragon@users.noreply.github.com>
2023-08-09 20:34:39 +0200

    add conversion suppot

--- plugins/modules/postgresql_owner.py
+++ plugins/modules/postgresql_owner.py
@@ -33,7 +33,8 @@ options:
     - I(obj_type=matview) is available since PostgreSQL 9.3.
     - I(obj_type=procedure) and I(obj_type=routine) are available since PostgreSQL 11.
     type: str
-    choices: [ database, function, matview, sequence, schema, table, tablespace, view, procedure, type, aggregate, routine, language, domain, collation ]
+    choices: [ database, function, matview, sequence, schema, table, tablespace, view, procedure,
+               type, aggregate, routine, language, domain, collation ]
     aliases:
     - type
   reassign_owned_by:
@@ -316,6 +317,9 @@ class PgOwnership(object):
         elif obj_type == 'collation':
             self.__set_collation_owner()
 
+        elif obj_type == 'conversion':
+            self.__set_conversion_owner()
+
     def __is_owner(self):
         """Return True if self.role is the current object owner."""
         if self.obj_type == 'table':
@@ -385,6 +389,12 @@ class PgOwnership(object):
             query = ("SELECT 1 FROM pg_collation AS c "
                      "JOIN pg_roles AS r ON c.collowner = r.oid "
                      "WHERE c.collname = %(obj_name)s "
+                     "AND r.rolname = %(role)s")
+
+        elif self.obj_type == 'conversion':
+            query = ("SELECT 1 FROM pg_conversion AS c "
+                     "JOIN pg_roles AS r ON c.conowner = r.oid "
+                     "WHERE c.conname = %(obj_name)s "
                      "AND r.rolname = %(role)s")
 
         if self.obj_type in ('function', 'aggregate', 'procedure', 'routine'):
@@ -487,6 +497,12 @@ class PgOwnership(object):
                                                       self.role)
         self.changed = exec_sql(self, query, return_bool=True)
 
+    def __set_conversion_owner(self):
+        """Set the conversion owner."""
+        query = 'ALTER CONVERSION %s OWNER TO "%s"' % (pg_quote_identifier(self.obj_name, 'table'),
+                                                       self.role)
+        self.changed = exec_sql(self, query, return_bool=True)
+
     def __role_exists(self, role):
         """Return True if role exists, otherwise return False."""
         query_params = {'role': role}
@@ -499,7 +515,7 @@ class PgOwnership(object):
 #
 
 VALID_OBJ_TYPES = ('database', 'function', 'matview', 'sequence', 'schema', 'table', 'tablespace', 'view',
-                   'procedure', 'type', 'aggregate', 'routine', 'language', 'domain', 'collation',)
+                   'procedure', 'type', 'aggregate', 'routine', 'language', 'domain', 'collation', 'conversion',)
 
 
 def main():
