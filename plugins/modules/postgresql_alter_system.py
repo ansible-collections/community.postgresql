d9589fef886709d41b26d971f83419a82c014569
Andrew Klychkov <aaklychkov@mail.ru>
2025-04-10 12:36:56 +0200

    Change headers in tests

--- plugins/modules/postgresql_alter_system.py
+++ plugins/modules/postgresql_alter_system.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# Copyright: (c) Andrew Klychkov (@Andersson007) <andrew.a.klychkov@gmail.com>
+# Copyright: (c) Andrei Klychkov (@Andersson007) <andrew.a.klychkov@gmail.com>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 from __future__ import absolute_import, division, print_function
