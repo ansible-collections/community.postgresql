78aa29c0d68505bb429f66cf518159edd434424c
Felix Fontein <felix@fontein.de>
2025-04-21 14:55:16 +0200

    Replace extra sanity test runner with antsibull-nox. (#836)

--- tests/utils/shippable/sanity.sh
+++ tests/utils/shippable/sanity.sh
@@ -2,23 +2,10 @@
 
 set -o pipefail -eux
 
-declare -a args
-IFS='/:' read -ra args <<< "$1"
-
-group="${args[1]}"
-
 if [ "${BASE_BRANCH:-}" ]; then
     base_branch="origin/${BASE_BRANCH}"
 else
     base_branch=""
-fi
-
-if [ "${group}" == "extra" ]; then
-    # ansible-galaxy -vvv collection install community.internal_test_tools
-    git clone --single-branch --depth 1 https://github.com/ansible-collections/community.internal_test_tools.git ../internal_test_tools
-
-    ../internal_test_tools/tools/run.py --color
-    exit
 fi
 
 # shellcheck disable=SC2086
