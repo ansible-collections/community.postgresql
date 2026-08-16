776f8b3302ea8da59c71492b091175e934b2b124
Florian R. Hölzlwimmer <git.ich@frhoelzlwimmer.de>
2026-08-16 15:57:28 +0000

    fix(module_utils): quote the object name in COMMENT ON
    set_comment interpolated the object name into the statement unquoted while
    parameterising only the comment value. A name containing a double quote
    produced invalid SQL and, under psycopg2, executed the remainder of the name:
    
      name: 'postgres" IS $$x$$; CREATE TABLE pwned(i int); --'
      -> COMMENT ON ROLE "postgres" IS $$x$$; CREATE TABLE pwned(i int); --" IS 'hi'
    
    Reachable since postgresql_user stopped rejecting such names. All callers pass
    a single unqualified name, so quoting there covers role, database, schema,
    extension, publication, subscription and tablespace alike.

--- plugins/module_utils/postgres.py
+++ plugins/module_utils/postgres.py
@@ -19,6 +19,8 @@ from os import environ
 
 from ansible.module_utils.common.text.converters import to_native
 from ansible.module_utils.basic import missing_required_lib
+from ansible_collections.community.postgresql.plugins.module_utils.database import \
+    pg_quote_name
 from ansible_collections.community.postgresql.plugins.module_utils.version import \
     LooseVersion
 
@@ -441,7 +443,8 @@ def set_comment(cursor, comment, obj_type, obj_name, check_mode=True, executed_q
         obj_type (str) -- Object type.
         executed_statements (list) -- List of executed state-modifying statements.
     """
-    query = 'COMMENT ON %s "%s" IS ' % (obj_type.upper(), obj_name)
+    # Every caller passes a single unqualified name, so it is quoted as one identifier.
+    query = 'COMMENT ON %s %s IS ' % (obj_type.upper(), pg_quote_name(obj_name))
 
     if not check_mode:
         cursor.execute(query + '%(comment)s', {'comment': comment})
